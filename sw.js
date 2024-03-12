dd03582783bc90feb9eaa5b9f24760ef893c2569
agentj2012 <156110508+agentj2012@users.noreply.github.com>
2024-03-12 19:14:31 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+// cach app
+const cacheName = 'joke-offline';
+const preCache = [
+    './',
+    './index.html',
+    './sw.js',   
+    './manifest.json'
+]
+
+
+
+self.addEventListener('install', e => {
+    console.log('The SW is installed!');
+    e.waitUntil(
+        caches.open(cacheName).then(cache => cache.addAll(preCache))
+    );
+
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(res => res || fetch(e.request))
+    );
+});
+
+
+
